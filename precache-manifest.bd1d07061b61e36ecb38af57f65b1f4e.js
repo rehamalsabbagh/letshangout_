8d78f10f65fe312bb3bafe8090fa1a14f4ceb86b
ralsabbagh <r.alsabbagh@tap.company>
2020-11-27 18:05:05 +0300

    Updates

---
+++ precache-manifest.bd1d07061b61e36ecb38af57f65b1f4e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8cd4971f91d07e03d42921ad3c810d1",
+    "url": "/letshangout/index.html"
+  },
+  {
+    "revision": "5b888c0c03ff6fb221ff",
+    "url": "/letshangout/static/css/main.8bc4da1f.chunk.css"
+  },
+  {
+    "revision": "6d2f52ec2ac2d949997a",
+    "url": "/letshangout/static/js/2.3c631b28.chunk.js"
+  },
+  {
+    "revision": "a5759f6343b8d54be3c1a5f4c039ec5b",
+    "url": "/letshangout/static/js/2.3c631b28.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5b888c0c03ff6fb221ff",
+    "url": "/letshangout/static/js/main.17c9e40a.chunk.js"
+  },
+  {
+    "revision": "5dda99d3506bb2bdebb6",
+    "url": "/letshangout/static/js/runtime-main.964ffc72.js"
+  }
+]);
