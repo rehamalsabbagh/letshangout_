ba5f27c1a3b2d395c8f575fe277b0e5bb77f7958
ralsabbagh <r.alsabbagh@tap.company>
2021-07-31 21:53:57 +0300

    update firebase

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,10 +1,9 @@
 export default {
-  apiKey: 'AIzaSyAP0Rc_Bt6Lb4MpOc4aD6lX-CvAxOxCtB4',
-  authDomain: 'letshangout-22c58.firebaseapp.com',
-  databaseURL: 'https://letshangout-22c58.firebaseio.com',
-  projectId: 'letshangout-22c58',
-  storageBucket: 'letshangout-22c58.appspot.com',
-  messagingSenderId: '652805895477',
-  appId: '1:652805895477:web:6ea45e974e0ab30842487c',
-  measurementId: 'G-9C5DQXKJ0J',
+  apiKey: "AIzaSyC4ykWs5dyJ5-MS83z3JCEP17KwonwwD9U",
+  authDomain: "letshangout01-70924.firebaseapp.com",
+  projectId: "letshangout01-70924",
+  storageBucket: "letshangout01-70924.appspot.com",
+  messagingSenderId: "83289691448",
+  appId: "1:83289691448:web:011e647f91ab41f3125dcd",
+  measurementId: "G-TS2XTQEPVP"
 };
