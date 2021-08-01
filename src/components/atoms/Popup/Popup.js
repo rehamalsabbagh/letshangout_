85a431e4dc531de44e87311b6bf66dfcb314b53b
ralsabbagh <r.alsabbagh@tap.company>
2021-08-02 02:38:56 +0300

    Add popup exit icon

--- src/components/atoms/Popup/Popup.js
+++ src/components/atoms/Popup/Popup.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Center from '../Center/Center';
 import { observer } from 'mobx-react-lite';
 import Container from '../Container/Container';
+import Image from '../Image/Image';
 
 function Poppup(props) {
   require('./Popup.css');
@@ -12,6 +13,10 @@ function Poppup(props) {
       className={className + props.popupStore.state}
       onClick={() => props.popupStore.setState('close')}
     >
+      <Image
+        className={'lho_popup_close'}
+        src={'https://flutter-apps.b-cdn.net/goCollect/test/cancel.svg'}
+      />
       <Center style={{ cursor: 'auto' }} onClick={(e) => e.stopPropagation()}>
         {props.popupStore.state === 'open' && props.children}
       </Center>
